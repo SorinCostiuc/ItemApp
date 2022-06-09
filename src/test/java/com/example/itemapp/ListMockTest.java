5373cdffb5191cf532761300f939e2c99a24a3fc
vbalan <vladbalan26@gmail.com>
2022-06-09 18:14:29 +0300

    added first mocked test

--- src/test/java/com/example/itemapp/ListMockTest.java
+++ src/test/java/com/example/itemapp/ListMockTest.java
@@ -1,13 +1,10 @@
 package com.example.itemapp;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -24,6 +21,6 @@ class ListMockTest {
         when(mockedList.size()).thenReturn(5);
         when(mockedList.get(1)).thenReturn("elemnt at index 1");
         assertEquals(5, mockedList.size());
-        assertEquals("elemnt at index 1", mockedList.get(1));
+        assertEquals("element at index 1", mockedList.get(1));
     }
 }
